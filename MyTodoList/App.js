f898bf60dda0c9bc67ab2dc0d51cee84dc4cf116
Tomas Tomov <tomastomov@yahoo.com>
2023-03-19 16:44:27 +0200

    Add TaskItem and TaskInputField and modify css for components

--- MyTodoList/App.js
+++ MyTodoList/App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Keyboard } from 'react-native';
+import { useState } from 'react'
+import TaskInputField from './TaskInputField'
+import TaskItem from './TaskItem'
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) {
+      return;
+    }
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (taskIndex) => {
+    if (taskIndex == null) {
+      return;
+    }
+
+    setTasks(tasks.filter((value, index) => index != taskIndex))
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>TODO List</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        {
+        tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -13,15 +44,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#1E1A3C',
   },
   heading: {
     color: '#fff',
     fontSize: 20,
     fontWeight: '600',
-    marginTop: 50,
+    marginTop: 30,
     marginBottom: 10,
-    marginLeft: 10,
+    marginLeft: 20,
   },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
